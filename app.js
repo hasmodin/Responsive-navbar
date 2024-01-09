56e573da1fb66d401676d4452a84484485165691
Hasmodin <hasmodin@gmail.com>
2024-01-09 11:09:10 +0300

    add files

---
+++ app.js
@@ -0,0 +1,9 @@
+function showSideBar() {
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = "flex";
+}
+
+function hideSidebar() {
+    const sidebar = document.querySelector(".siderbar");
+    sidebar.style.display = "none";
+}
